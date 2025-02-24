90b16904d44570e7d59e5d499eb443d3368b2532
abdullahmukri25@gmail.com <91679434+Abdullah-Github25@users.noreply.github.com>
2025-02-25 02:23:50 +0530

    feat: Contribution chart added

---
+++ Components/Github.tsx
@@ -0,0 +1,14 @@
+import Image from "next/image"
+
+const Github = () => {
+    return (
+        <Image src="https://ghchart.rshah.org/00ff00/abhub23"
+            alt="Github contribution Chart"
+            width={900}
+            height={600}
+            unoptimized={true}
+            />
+    )
+}
+
+export default Github
