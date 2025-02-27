b2f3d68b40e68ab40009515792b0d38c0ffcccb3
abdullahmukri25@gmail.com <91679434+Abdullah-Github25@users.noreply.github.com>
2025-02-28 00:08:42 +0530

    feat: error page added

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,19 +1,16 @@
-'use client'
+"use client";
 
 import { useRouter } from "next/navigation";
 
 const ErrorPage = () => {
   const router = useRouter();
   const handleHome = () => {
-    router.push("/")
+    router.push("/");
   };
 
   return (
-    <div className="bg-white dark:bg-black flex flex-col justify-center items-center h-screen">
-      <div className="text-3xl text-black dark:text-white p-2 mb-1.5">
-        {" "}
-        PAGE NOT FOUND - 404{" "}
-      </div>
+    <div className="bg-white flex flex-col justify-center items-center h-screen">
+      <div className="text-3xl text-black p-2 mb-1.5">PAGE NOT FOUND - 404 </div>
       <div className="p-2">
         <button
           className="border-4 px-3 py-2 rounded-2xl bg-white cursor-pointer text-lg dark:border-gray-600 dark:bg-gray-800 dark:text-white "
