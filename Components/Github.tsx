12939ad14c312c9fbba5bc2e80069d24b5ab7cf9
abdullahmukri25@gmail.com <91679434+Abdullah-Github25@users.noreply.github.com>
2025-02-27 11:10:54 +0530

    feat: components added

--- Components/Github.tsx
+++ Components/Github.tsx
@@ -1,14 +1,15 @@
-import Image from "next/image"
+import Image from "next/image";
 
 const Github = () => {
-    return (
-        <Image src="https://ghchart.rshah.org/00ff00/abhub23"
-            alt="Github contribution Chart"
-            width={900}
-            height={600}
-            unoptimized={true}
-            />
-    )
-}
+  return (
+    <Image
+      src="https://ghchart.rshah.org/00ff00/abhub23"
+      alt="Github contribution Chart"
+      width={900}
+      height={600}
+      unoptimized={true}
+    />
+  );
+};
 
-export default Github
+export default Github;
