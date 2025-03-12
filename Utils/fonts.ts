e061b4b56dadf6469eb52d16ad61d37540faa0d5
abdullahmukri25@gmail.com <91679434+Abdullah-Github25@users.noreply.github.com>
2025-03-12 07:53:14 +0530

    fix: Next13+ have inbuilt fonts no need to install anything

--- Utils/fonts.ts
+++ Utils/fonts.ts
@@ -1,11 +1,10 @@
-import { NextFont } from '@next/font';
 import { Bricolage_Grotesque, Press_Start_2P } from 'next/font/google';
 
-const Bricolage_font: NextFont = Bricolage_Grotesque({
+const Bricolage_font= Bricolage_Grotesque({
   subsets: ['latin'],
 });
 
-const Press_Start_2P_font: NextFont = Press_Start_2P({
+const Press_Start_2P_font = Press_Start_2P({
   weight: '400',
   subsets: ['latin']
 });
