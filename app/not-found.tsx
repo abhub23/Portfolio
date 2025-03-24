20db9ade7fea250614ae33b2036f003f45b5ddc2
abdullahmukri25@gmail.com <91679434+Abdullah-Github25@users.noreply.github.com>
2025-03-24 15:23:45 +0530

    refactor

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,16 +1,17 @@
-"use client";
+'use client';
 
-import { useRouter } from "next/navigation";
+import { useRouter } from 'next/navigation';
+import { Bricolage } from '@/Utils/fonts';
 
 const ErrorPage = () => {
   const router = useRouter();
   const handleHome = () => {
-    router.push("/");
+    router.push('/');
   };
 
   return (
-    <div className="bg-white flex flex-col justify-center items-center h-screen">
-      <div className="text-3xl text-black p-2 mb-1.5">PAGE NOT FOUND - 404 </div>
+    <div className={`${Bricolage} bg-white flex flex-col justify-center items-center h-screen`}>
+      <div className="text-4xl text-black p-2 mb-1.5">PAGE NOT FOUND - 404 </div>
       <div className="p-2">
         <button
           className="border-4 px-3 py-2 rounded-2xl bg-white cursor-pointer text-lg dark:border-gray-600 dark:bg-gray-800 dark:text-white "
