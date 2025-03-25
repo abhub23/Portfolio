a33b743093bcb012f5790fb410a5749a8b6f2179
abdullahmukri25@gmail.com <91679434+Abdullah-Github25@users.noreply.github.com>
2025-03-25 22:25:32 +0530

    fix: footer fixed

--- Components/Footer.tsx
+++ Components/Footer.tsx
@@ -1,5 +1,18 @@
+import Link from 'next/link';
+
 const Footer = () => {
-  return <></>;
+  return (
+    <div className="h-10 bg-transparent flex font-extralight justify-center font-mono">
+      Designed and Developed by
+      <Link
+        className="text-blue-400 underline ml-2.5"
+        href="https://x.com/abdullah_twt23"
+        target="_black"
+      >
+        Abdullah Mukri
+      </Link>
+    </div>
+  );
 };
 
 export default Footer;
