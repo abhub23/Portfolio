0d66eec8e7177adbc0ebc163b5ec2d7be725ee38
abdullahmukri25@gmail.com <91679434+Abdullah-Github25@users.noreply.github.com>
2025-04-10 01:53:48 +0530

    zustand good to go

--- store/DarkmodeProvider.tsx
+++ store/DarkmodeProvider.tsx
@@ -1,7 +1,7 @@
-'use client'
+'use client';
 
 import { ReactNode, useEffect } from 'react';
-import { useDarkmode } from './useDarkmode';
+import { useDarkmode } from '@/store/useDarkmode';
 
 export const DarkmodeProvider = ({ children }: { children: ReactNode }) => {
   const initializeTheme = useDarkmode((state) => state.initializeTheme);
