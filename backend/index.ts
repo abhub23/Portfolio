140a4eea71dfe8a6e230074cc0a1585eef168057
abdullahmukri25@gmail.com <91679434+Abdullah-Github25@users.noreply.github.com>
2025-04-11 23:57:51 +0530

    backend chore

--- backend/index.ts
+++ backend/index.ts
@@ -1,19 +1,20 @@
-import express from 'express'
-import cors from 'cors'
-const Port = 8000
+import express from 'express';
+import cors from 'cors';
+const Port = 8000;
 
-const app = express()
-app.use(cors({
-    origin: "*"
-}))
+const app = express();
+app.use(
+  cors({
+    origin: '*',
+  })
+);
 
-app.post('/send', (req , res) => {
-        res.status(200).json({
-            message: "Success"
-        })
-    
-})
+app.post('/send', (req, res) => {
+  res.status(200).json({
+    message: 'Success',
+  });
+});
 
 app.listen(Port, () => {
-    console.log(`Server listening on port ${Port} `)
-})
+  console.log(`Server listening on port ${Port} `);
+});
