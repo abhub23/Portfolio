9c1061e864297fb9cce71fd23c8c5a00c476723b
abdullahmukri25@gmail.com <91679434+Abdullah-Github25@users.noreply.github.com>
2025-04-12 04:49:28 +0530

    fix: footer responsiveness fixed

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,11 +1,15 @@
 import Link from 'next/link';
+import { Bricolage } from '@/utils/fonts';
+
 
 const Footer = () => {
   return (
-    <div className="h-10 bg-transparent flex font-extralight justify-center font-mono">
+    <div
+      className={`h-10 bg-transparent lg:text-[16px] text-[12px] flex justify-center items-center mb-2 dark:text-white ${Bricolage}`}
+    >
       Designed and Developed by
       <Link
-        className="text-blue-400 underline ml-2.5"
+        className="text-rose-300 ml-1 hover:underline"
         href="https://x.com/abdullah_twt23"
         target="_black"
       >
