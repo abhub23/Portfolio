7f7950280184d757c86dc35762f02c09e297815e
abdullahmukri25@gmail.com <91679434+Abdullah-Github25@users.noreply.github.com>
2025-04-18 21:38:29 +0530

    refactor

--- next.config.ts
+++ next.config.ts
@@ -2,15 +2,14 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   allowedDevOrigins: [
-    'http://192.168.0.104:3000',  // IP of your computer in the local network
+    'http://192.168.0.104:3000', 
     'http://localhost:3000',
     'http://127.0.0.1:3000',
-    'http://103.221.248.91:3000' // Make sure to replace <your-computer-ip> with your actual computer's IP address
+    'http://103.221.248.91:3000'
   ],   
   images: {
     domains: ['ghchart.rshah.org'],
   },
-  /* config options here */
 };
 
 export default nextConfig;
