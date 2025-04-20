cbca70db1f701027117ac035c903b08f2881ff6a
abdullahmukri25@gmail.com <91679434+Abdullah-Github25@users.noreply.github.com>
2025-04-21 01:33:23 +0530

    fix: size fix

--- components/Button.tsx
+++ components/Button.tsx
@@ -17,9 +17,9 @@ const Button: React.FC<Btntype> = ({ prop, onClick }) => {
       className="group cursor-pointer group-hover:before:duration-500 group-hover:after:duration-500 after:duration-500 hover:border-rose-300 
       hover:before:[box-shadow:_20px_20px_20px_30px_#a21caf] duration-500 before:duration-500 hover:duration-500 underline underline-offset-2 
       hover:after:-right-8 hover:before:right-12 hover:before:-bottom-8 hover:before:blur hover:underline hover:underline-offset-4  origin-left 
-      hover:decoration-2 hover:text-rose-300 relative bg-neutral-800 lg:h-13 lg:w-55 h-12 w-40  text-left p-3 text-gray-50 lg:text-[16px] text-xs font-bold 
-      rounded-lg  overflow-hidden  before:absolute before:w-12 before:h-12 before:content[''] before:right-1 before:top-1 before:z-10 
-      before:bg-violet-500 before:rounded-full before:blur-lg  after:absolute after:z-10 after:w-20 after:h-20 after:content['']  
+      hover:decoration-2 hover:text-rose-300 relative bg-neutral-800 lg:h-13 lg:w-50 h-12 w-40  text-left p-3 text-gray-50 lg:text-[14px] text-xs font-bold 
+      rounded-lg  overflow-hidden  before:absolute before:w-10 before:h-10 before:content[''] before:right-1 before:top-1 before:z-10 
+      before:bg-violet-500 before:rounded-full before:blur-lg  after:absolute after:z-10 after:w-18 after:h-18 after:content['']  
       after:bg-rose-300 after:right-8 after:top-3 after:rounded-full after:blur-lg"
       onClick={onClick}
     >
