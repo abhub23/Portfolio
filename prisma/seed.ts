a95408f0591f80980459e1735bc215506e57136e
abdullahmukri25@gmail.com <91679434+Abdullah-Github25@users.noreply.github.com>
2025-04-20 22:30:07 +0530

    feat: seed file for seeding the prisma db

---
+++ prisma/seed.ts
@@ -0,0 +1,30 @@
+{ /* 
+
+What is seed.ts?
+The seed.ts file is used to add sample data (like test users, posts, etc.) into your database. This is called "seeding" your database.
+
+Think of it like this:
+“I just created an empty database. Now I want to fill it with some sample data so I can test my app.”
+That’s what seed.ts is for. 
+
+*/}
+
+import { PrismaClient } from '@prisma/client'
+const prisma = new PrismaClient()
+
+async function main() {
+  await prisma.users.create({
+    data: { id: 1, name: 'test', email: 'test@gmail.com', message: 'testing' },
+  })
+}
+
+main()
+.then(() => {
+    console.log('prisma init data')
+})
+.catch((error) => {
+    console.error('Error occcured in prisma: ',error)
+})
+.finally(() => {
+    prisma.$disconnect()
+})
