87c8a2192610e57f57c6c67d3eafe3629cebd596
abdullahmukri25@gmail.com <91679434+Abdullah-Github25@users.noreply.github.com>
2025-04-23 01:08:28 +0530

    refactor: code refactor

--- components/Connectwithme.tsx
+++ components/Connectwithme.tsx
@@ -3,7 +3,7 @@
 import { useRef } from "react";
 import { JSX } from "react";
 import { Bricolage } from "@/utils/fonts";
-import { useName, useEmail, useMessage } from "@/store/Connectwithme";
+import { useEmail, useMessage } from "@/store/Connectwithme";
 import axios from "axios";
 
 const Connectwithme: React.FC = (): JSX.Element => {
@@ -23,24 +23,25 @@ const Connectwithme: React.FC = (): JSX.Element => {
 
     const handleMessage = async () => {
         try {
-            console.log(name, email, message)
-            await axios.post('http://localhost:8000/send', { name, email, message })
-            console.log("DONE");
+            await axios.post('http://localhost:8000/send', { email, message })
         } catch (error) {
             console.log('Error occured while sending message', error)
         }
     }
 
     return (
-        <div className={` ${Bricolage} h-[250px] w-72 flex flex-col items-start gap-4 ml-100 p-4`}>
+        <div className={` ${Bricolage} h-[300px] w-72 flex flex-col items-start ml-100 p-4`}>
+
             {/* <input type="text" placeholder="Name" required={true} onChange={(e) => setName(e.currentTarget.value)}
                 className="border-black dark:border-white bg-white dark:bg-black p-2 border-2 h-[35px] w-[620px] rounded-[4px]" /> */}
-            <input type="text" placeholder="Email" required={true} onChange={(e) => setEmail(e.currentTarget.value)}
-                className="border-black dark:border-white bg-white dark:bg-black p-2 border-2 h-[35px] w-[705px] rounded-[4px]" />
-            <textarea onInput={handleInput} ref={textareaRef} placeholder="Message" required={true} onChange={(e) => setMessage(e.currentTarget.value)}
-                className="border-black dark:border-white bg-white dark:bg-black p-2 border-2 min-h-[42px] h-[42px] overflow-hidden min-w-[705px] w-[705px] max-w-[705px] resize rounded-[4px]" />
+            <div className="p-[2px]">Email </div>
+            <input type="text" placeholder="abdullahmukri25@gmail.com" required={true} onChange={(e) => setEmail(e.currentTarget.value)}
+                className="border-black dark:border-white bg-white dark:bg-black text-[14px] p-2 mb-[12px] border-2 h-[32px] w-[705px] rounded-[4px]" />
+            <div className="p-[2px]">Message</div>            
+            <textarea onInput={handleInput} ref={textareaRef} placeholder="Hey, I have this cool idea, could you help me build it ?" required={true} onChange={(e) => setMessage(e.currentTarget.value)}
+                className="border-black dark:border-white bg-white dark:bg-black text-[14px] mb-[20px] p-2 border-2 min-h-[60px] h-[42px] overflow-hidden min-w-[705px] w-[705px] max-w-[705px] resize rounded-[4px]" />
             <button
-                className='border-black bg-black dark:bg-white dark:border-white text-[14px] text-white dark:text-black border-2 h-9 w-[705px] rounded-[4px] mt-1 cursor-pointer hover:bg-stone-900'
+                className='border-black bg-black dark:bg-white dark:border-white text-[14px] text-white dark:text-black border-2 h-9 w-[705px] rounded-[4px] mt-1 cursor-pointer hover:bg-stone-900 dark:hover:bg-gray-100'
                 onClick={handleMessage}> Send Message </button>
         </div>
     )
