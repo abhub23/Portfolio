64fac0a2d7c789257bd8dfa6c7bb7d783cbb6b51
abdullahmukri25@gmail.com <91679434+Abdullah-Github25@users.noreply.github.com>
2025-04-24 00:41:27 +0530

    feat: zod validation added and (req, res) types fixed

--- backend/index.ts
+++ backend/index.ts
@@ -1,5 +1,8 @@
 import express from 'express';
+import type { Request } from 'express';
 import cors from 'cors';
+import { prisma } from './lib/prisma';
+import { z } from 'zod'
 const Port = 8000;
 
 const app = express();
@@ -8,13 +11,44 @@ app.use(
     origin: '*',
   })
 );
+app.use(express.json())
 
-app.post('/send', (req, res) => {
-  res.status(200).json({
-    message: 'Success',
-  });
+const UserSchema = z.object({
+  email: z.string().email().max(50),
+  message: z.string()
+})
+
+app.post('/api/sendmessage', async (req: Request, res: any) => {
+
+  const InputValidation = UserSchema.safeParse(req.body)
+  
+  if(!InputValidation.success){
+    return res.status(400).json({
+      message: "Zod validation failed",
+      Error: InputValidation.error.flatten().fieldErrors
+    })
+  }
+  const { email, message } =InputValidation.data;
+
+  try {
+    await prisma.users.create({
+      data: { email: email, message: message },
+    });
+
+    return res.status(200).json({
+      Success: true,
+      Message: "Data added to the DB"
+    });
+
+  } catch (error) {
+    console.error('Error occured while adding data to DB : ', error)
+
+    return res.status(500).json({
+      Success: false
+    })
+  }
 });
 
 app.listen(Port, () => {
-  console.log(`Server listening on port ${Port} `);
+  console.log(`Server Listening on Port: ${Port}`);
 });
