4ad928d3df6dcbc36a48a1684ff0da2bf4af4688
abdullahmukri25@gmail.com <91679434+Abdullah-Github25@users.noreply.github.com>
2025-04-25 23:40:44 +0530

    refactor: some ui changes

--- components/Github.tsx
+++ components/Github.tsx
@@ -1,11 +1,11 @@
-import { Bricolage } from '@/Utils/fonts';
+import { Bricolage } from '@/utils/fonts';
 import Image from 'next/image';
-import { JSX } from 'react';
+import type { JSX } from 'react';
 
 const Github: React.FC = (): JSX.Element => {
   return (
-    <div className="flex flex-col items-center mb-12 mt-14">
-      <p className={`${Bricolage} mb-5 text-4xl font-bold `}>Github Contributions</p>
+    <div className="flex flex-col p-2 items-center mb-12 mt-14">
+      <p className={`${Bricolage} lg:mb-5 mb-2 lg:text-[34px] text-[24px] font-semibold `}>Github Contributions</p>
       <Image
         src="https://ghchart.rshah.org/216E39/abhub23"
         alt="Github contribution Chart"
