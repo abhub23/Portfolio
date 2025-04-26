232ae5bde71f42227f23f7d2422690450fad2390
abdullahmukri25@gmail.com <91679434+Abdullah-Github25@users.noreply.github.com>
2025-04-27 00:55:19 +0530

    fix: fixed responsiveness on mobile

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -19,9 +19,9 @@ const ErrorPage = () => {
       transition={{ duration: 0.5, ease: 'easeOut' }}
       className='flex flex-col justify-center items-center'
       >
-        <p className="text-4xl text-black dark:text-white p-2 mb-1.5">PAGE NOT FOUND - 404 </p>
+        <p className="lg:text-[36px] text-[22px] text-black dark:text-white p-2 mb-1.5">PAGE NOT FOUND - 404 </p>
         <button
-          className="px-3 py-2 p-2 rounded-md bg-black text-white cursor-pointer text-[14px] dark:bg-white dark:text-black "
+          className="px-3 py-2 p-2 rounded-md lg:h-[36px] lg:w-[120px] h-[28px] w-[100px] bg-black text-white cursor-pointer lg:text-[14px] text-[10px] dark:bg-white dark:text-black "
           onClick={handleHome}
         >
           Go to Home
