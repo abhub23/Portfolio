dd4d19939647384d968cdb2906fc190fc702b76b
abdullahmukri25@gmail.com <91679434+Abdullah-Github25@users.noreply.github.com>
2025-04-28 22:51:47 +0530

    refactor

--- utils/Emptyalert.ts
+++ utils/Emptyalert.ts
@@ -3,7 +3,7 @@ import Swal from 'sweetalert2';
 const sweetalert = () =>
   Swal.fire({
     icon: 'error',
-    title: 'Input fields missing...',
+    title: 'Input fields missing',
     text: 'Make sure to fill up both the fields',
   });
 
