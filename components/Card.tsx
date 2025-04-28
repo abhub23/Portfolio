38cba1aecd4496929e751feaef3516d97b022c15
abdullahmukri25@gmail.com <91679434+Abdullah-Github25@users.noreply.github.com>
2025-04-28 22:49:10 +0530

    fix: skills display fix, used flex wrap instead of grids, it divids the space equally, also remover w-fit utility

--- components/Card.tsx
+++ components/Card.tsx
@@ -9,12 +9,14 @@ type Cardprops = {
   Desc: string;
   SrcLink: string;
   WebLink: string;
+  Skills: string[];
 };
 
-const Card: React.FC<Cardprops> = ({ Title, Desc, SrcLink, WebLink }): JSX.Element => {
+const Card: React.FC<Cardprops> = ({ Title, Desc, SrcLink, WebLink, Skills }): JSX.Element => {
   return (
-    <article className="relative flex w-[350px] flex-col items-start justify-between border-[1px] rounded-sm  border-zinc-300/80 dark:border-zinc-800 bg-gradient-to-b from-white via-gray-100 to-gray-200 dark:from-black dark:via-black dark:to-black p-6 shadow-[5px_5px_0_0_oklch(55.2%_0.016_285.938)] dark:shadow-[5px_5px_0_0_oklch(96.7%_0.001_286.375)] transition-all duration-500 ease-in-out transform hover:scale-105 hover:bg-gradient-to-b hover:from-gray-200 hover:to-white dark:hover:from-black dark:hover:to-black hover:shadow-[7px_7px_0_0_oklch(55.2%_0.016_285.938)] dark:hover:shadow-[7px_7px_0_0_oklch(96.7%_0.001_286.375)]"> 
-      <ShineBorder className='rounded-sm' shineColor={['oklch(64.5% 0.246 16.439)','oklch(85.2% 0.199 91.936)','oklch(76.8% 0.233 130.85)']} />
+    <article className="relative flex w-[340px] flex-col items-start justify-between border-[1px] rounded-sm  border-zinc-300/80 dark:border-zinc-800 bg-gradient-to-b from-white via-gray-100 to-gray-200 dark:from-black dark:via-black dark:to-black p-6 transition-all duration-500 ease-in-out transform hover:bg-gradient-to-b hover:from-gray-200 hover:to-white dark:hover:from-black dark:hover:to-black ">
+      {/*<ShineBorder className='rounded-sm' shineColor={['oklch(64.5% 0.246 16.439)','oklch(85.2% 0.199 91.936)','oklch(76.8% 0.233 130.85)']} />*/}
+      <ShineBorder className='rounded-sm' shineColor={['oklch(71.8% 0.202 349.761)', 'oklch(74% 0.238 322.16)', 'oklch(70.2% 0.183 293.541)']} />
       <div className="group relative">
         <h3 className=" mt-3 text-2xl font-black uppercase leading-6 text-black dark:text-white transition-all duration-500 ease-in-out transform hover:scale-105 hover:text-rose-600">
           <a href="#">
@@ -28,20 +30,19 @@ const Card: React.FC<Cardprops> = ({ Title, Desc, SrcLink, WebLink }): JSX.Eleme
           {Desc}
         </p>
       </div>
-      <div className="relative mt-8 flex items-center gap-x-2">
-        <div className="text-sm leading-6">
-          <p className="font-black text-black dark:text-white transition-all duration-500 ease-in-out transform hover:scale-110">
-            <a className="hover:underline hover:text-red-500" href="#">
-              <span className="absolute inset-0" />
-              -ELON MUSK-
-            </a>
+
+      <div className="flex gap-1 px-3 mt-6 flex-wrap">
+        {Skills.map(el => (
+          <p
+            key={el}
+            className={`text-[11px] rounded-xs font-semibold p-[2px] px-[6px] border-1 border-black/50 dark:border-white/90 h-[20px] text-black dark:text-white hover:dark:text-black hover:text-white hover:bg-black dark:hover:bg-white transition-all duration-500 ease-in-out cursor-pointer ${Bricolage}`}
+          >
+            {el}
           </p>
-          <p className="font-bold text-gray-700 dark:text-white/90 transition-all duration-500 ease-in-out transform hover:text-gray-500">
-            Chief Memelord
-          </p>
-        </div>
+        ))}
+
       </div>
-      <div className="mt-2 mb-[-10px] flex items-center gap-x-2 text-xs">
+      <div className="mt-10 ml-[13px] mb-[-10px] flex items-center gap-x-2 text-xs">
         <GithubBtn Srclink={SrcLink} />
         <WebsiteBtn Weblink={WebLink} />
       </div>
