5febaa78266ada1ea32735a3e5a54df37f3d7727
abdullahmukri25@gmail.com <91679434+Abdullah-Github25@users.noreply.github.com>
2025-04-29 22:08:28 +0530

    fix: ui and display properties

--- components/Card.tsx
+++ components/Card.tsx
@@ -14,35 +14,34 @@ type Cardprops = {
 
 const Card: React.FC<Cardprops> = ({ Title, Desc, SrcLink, WebLink, Skills }): JSX.Element => {
   return (
-    <article className="relative flex w-[340px] flex-col items-start justify-between border-[1px] rounded-sm  border-zinc-300/80 dark:border-zinc-800 bg-gradient-to-b from-white via-gray-100 to-gray-200 dark:from-black dark:via-black dark:to-black p-6 transition-all duration-500 ease-in-out transform hover:bg-gradient-to-b hover:from-gray-200 hover:to-white dark:hover:from-black dark:hover:to-black ">
+    <article className="relative flex w-[340px] flex-col items-center justify-between border-[1px] rounded-sm  border-zinc-300/80 dark:border-zinc-800 bg-gradient-to-b from-white via-gray-100 to-gray-200 dark:from-black dark:via-black dark:to-black p-6 transition-all duration-500 ease-in-out transform hover:bg-gradient-to-b hover:from-gray-200 hover:to-white dark:hover:from-black dark:hover:to-black ">
       {/*<ShineBorder className='rounded-sm' shineColor={['oklch(64.5% 0.246 16.439)','oklch(85.2% 0.199 91.936)','oklch(76.8% 0.233 130.85)']} />*/}
       <ShineBorder className='rounded-sm' shineColor={['oklch(71.8% 0.202 349.761)', 'oklch(74% 0.238 322.16)', 'oklch(70.2% 0.183 293.541)']} />
-      <div className="group relative">
-        <h3 className=" mt-3 text-2xl font-black uppercase leading-6 text-black dark:text-white transition-all duration-500 ease-in-out transform hover:scale-105 hover:text-rose-600">
-          <a href="#">
-            <span className="absolute inset-0 max-w-xs" />
+      <div className="group w-[320px] relative">
+        <h3 className={` mt-3 text-2xl font-black ml-[8px] uppercase leading-6 text-black dark:text-white transition-all duration-500 ease-in-out transform hover:scale-105 hover:text-rose-600  ${Bricolage}`}>
+          <a href={WebLink}>
+            <span className={`absolute inset-0 max-w-xs  ${Bricolage}`} />
             {Title}
           </a>
         </h3>
-        <p
-          className={`text-sm mt-5 border-l-4 border-red-500 pl-4 leading-6 text-black dark:text-white text-justify transition-all duration-500 ease-in-out transform ${Bricolage} `}
-        >
+        <p className={`text-[13px] mt-5 px-2 text-black dark:text-white text-justify transition-all duration-500 ease-in-out transform ${Bricolage} `}>
           {Desc}
         </p>
       </div>
 
-      <div className="flex gap-1 px-3 mt-6 flex-wrap">
+      <div className="flex w-[330] gap-[2px] gap-y-[4px] px-3 mt-6 flex-wrap">
         {Skills.map(el => (
           <p
             key={el}
-            className={`text-[11px] rounded-xs font-semibold p-[2px] px-[6px] border-1 border-black/50 dark:border-white/90 h-[20px] text-black dark:text-white hover:dark:text-black hover:text-white hover:bg-black dark:hover:bg-white transition-all duration-500 ease-in-out cursor-pointer ${Bricolage}`}
+            className={`text-[11px] rounded-xs ml-[2px] p-[2px] px-[4px] border-1 border-black/50 dark:border-white/90 h-[20px] text-black dark:text-white hover:dark:text-black hover:text-white hover:bg-black dark:hover:bg-white transition-all duration-300 ease-in-out cursor-pointer ${Bricolage}`}
           >
             {el}
           </p>
         ))}
 
       </div>
-      <div className="mt-10 ml-[13px] mb-[-10px] flex items-center gap-x-2 text-xs">
+      
+      <div className="mt-10 mr-[130px] flex gap-x-2 text-xs">
         <GithubBtn Srclink={SrcLink} />
         <WebsiteBtn Weblink={WebLink} />
       </div>
