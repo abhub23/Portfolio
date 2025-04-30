ab973433e7101d64eb4988c08f303cebe0767b30
abdullahmukri25@gmail.com <91679434+Abdullah-Github25@users.noreply.github.com>
2025-04-30 23:26:59 +0530

    chore: used prettier

--- next.config.ts
+++ next.config.ts
@@ -2,11 +2,11 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   allowedDevOrigins: [
-    'http://192.168.0.104:3000', 
+    'http://192.168.0.104:3000',
     'http://localhost:3000',
     'http://127.0.0.1:3000',
-    'http://103.221.248.91:3000'
-  ],   
+    'http://103.221.248.91:3000',
+  ],
   images: {
     domains: ['ghchart.rshah.org'],
   },
--- store/Connectwithme.ts
+++ store/Connectwithme.ts
@@ -1,30 +1,28 @@
-import {create} from 'zustand'
+import { create } from 'zustand';
 
 type Name = {
-    name: string;
-    setName: (newname: string) => void
-}
+  name: string;
+  setName: (newname: string) => void;
+};
 export const useName = create<Name>((set) => ({
-    name: "",
-    setName: (newname: string) => set({name: newname })
-}))
-
+  name: '',
+  setName: (newname: string) => set({ name: newname }),
+}));
 
 type Email = {
-    email: string;
-    setEmail: (newemail: string) => void
-}
+  email: string;
+  setEmail: (newemail: string) => void;
+};
 export const useEmail = create<Email>((set) => ({
-    email: "",
-    setEmail: (newemail: string) => set({email: newemail })
-}))
-
+  email: '',
+  setEmail: (newemail: string) => set({ email: newemail }),
+}));
 
 type Message = {
-    message: string;
-    setMessage: (newmessage: string) => void
-}
+  message: string;
+  setMessage: (newmessage: string) => void;
+};
 export const useMessage = create<Message>((set) => ({
-    message: "",
-    setMessage: (newmessage: string) => set({message: newmessage })
-}))
+  message: '',
+  setMessage: (newmessage: string) => set({ message: newmessage }),
+}));
