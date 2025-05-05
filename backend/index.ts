3e441103b047bc9cc38b4a9f5777157b01218964
abdullahmukri25@gmail.com <91679434+Abdullah-Github25@users.noreply.github.com>
2025-05-05 16:42:52 +0530

    feat: resend sdk integrated

--- backend/index.ts
+++ backend/index.ts
@@ -2,7 +2,8 @@ import express from 'express';
 import type { Request } from 'express';
 import cors from 'cors';
 import { prisma } from './lib/prisma';
-import { z } from 'zod'
+import { z } from 'zod';
+import { resend } from './lib/resend';
 const Port = 8000;
 
 const app = express();
@@ -11,41 +12,46 @@ app.use(
     origin: '*',
   })
 );
-app.use(express.json())
+app.use(express.json());
 
 const UserSchema = z.object({
   email: z.string().email().max(50),
-  message: z.string()
-})
+  message: z.string(),
+});
 
 app.post('/api/sendmessage', async (req: Request, res: any) => {
+  const InputValidation = UserSchema.safeParse(req.body);
 
-  const InputValidation = UserSchema.safeParse(req.body)
-  
-  if(!InputValidation.success){
+  if (!InputValidation.success) {
     return res.status(400).json({
-      message: "Zod validation failed",
-      Error: InputValidation.error.flatten().fieldErrors
-    })
+      message: 'Zod validation failed',
+      Error: InputValidation.error.flatten().fieldErrors,
+    });
   }
-  const { email, message } =InputValidation.data;
+  const { email, message } = InputValidation.data;
 
   try {
     await prisma.users.create({
       data: { email: email, message: message },
     });
 
-    return res.status(200).json({
-      Success: true,
-      Message: "Data added to the DB"
+    await resend.emails.send({
+      from: email,
+      to: 'abdullahmukri25@gmail.com',
+      subject: `Hey Abdullah you got a message from ${email}`,
+      html: `<p>Hi Abdullah, you got a message from ${email} <strong>${message}</strong>!</p>`
     });
 
+    return res.status(200).json({
+      Success: true,
+      Message: 'Data added to the DB',
+    });
   } catch (error) {
-    console.error('Error occured while adding data to DB : ', error)
+    console.error('Error occured while adding data to DB : ', error);
 
     return res.status(500).json({
-      Success: false
-    })
+      Success: false,
+    });
   }
 });
 
