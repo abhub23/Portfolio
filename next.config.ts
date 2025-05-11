654efae33bd0a14c0797da3299188cf4c199119f
abdullahmukri25@gmail.com <91679434+Abdullah-Github25@users.noreply.github.com>
2025-05-11 23:52:27 +0530

    all origins allowed

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,7 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  allowedDevOrigins: [
-    'http://192.168.0.104:3000',
-    'http://localhost:3000',
-    'http://127.0.0.1:3000',
-    'http://103.221.248.91:3000',
-  ],
+  allowedDevOrigins: ['*'],
   images: {
     domains: ['ghchart.rshah.org'],
   },
