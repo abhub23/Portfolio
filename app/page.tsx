9c5bffe74cdb61a7d4ba0350835f5577d1b2e645
abdullahmukri25@gmail.com <91679434+Abdullah-Github25@users.noreply.github.com>
2025-05-14 21:41:58 +0530

    feat: init

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,28 @@
-import Github from "@/Components/Github";
-import { Press_Start_2P } from "next/font/google";
-import Navbar from "@/Components/Navbar";
+'use client';
 
-const pressStart2P = Press_Start_2P({
-  weight: '400',
-  
-})
+import Github from '@/components/Github';
+import Footer from '@/components/Footer';
+import Introsec from '@/components/Introsec';
+import Projects from './projects/page';
+import Connectwithme from '@/components/Connectwithme';
+import { motion } from 'motion/react';
+import Skills from '@/components/Skills';
 
 export default function Home() {
   return (
     <>
-      <div className={pressStart2P.className} >
-      
-      </div>
+      <motion.div
+        initial={{ opacity: 0, y: 30 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.6, ease: 'easeOut' }}
+      >
+        <Introsec />
+        <Projects />
+        <Github />
+        <Skills/>
+        <Connectwithme />
+        <Footer />
+      </motion.div>
     </>
   );
 }
