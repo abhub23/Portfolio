b2ab10aa0082fb80543d4203f7cf6f8dba8d6f13
abdullahmukri25@gmail.com <91679434+Abdullah-Github25@users.noreply.github.com>
2025-05-16 22:36:37 +0530

    projects page added

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,20 +1,121 @@
-import Card from '@/Components/Card';
-import { Bricolage } from '@/Utils/fonts';
+'use client'
+
+import Card from '@/components/Card';
+import { Bricolage } from '@/utils/fonts';
+import { usePathname } from 'next/navigation';
 
 const Projects: React.FC = () => {
+  const pathname = usePathname()
   return (
     <>
-      <p className={`text-4xl mt-8 font-bold flex justify-center p-4 mb-4 ${Bricolage}`}> Projects</p>
-      <div className="grid grid-cols-2 gap-6 mx-96 justify-center">
+      <p
+        className={`lg:text-[34px] text-[24px] ${pathname == '/projects' ? 'mt-[150px]' :'mt-[32px]' } font-semibold flex justify-center p-2 lg:mb-4  ${Bricolage}`}
+      >
+        {' '}
+        Projects
+      </p>
+      <div className="grid lg:grid-cols-2 gap-4 lg:mx-104 justify-center">
+        <Card
+          Title="To Be Decided"
+          Desc="Compare your Github Metrics with the best in the Bizz. Torvalds, Karpathy, Harkirat and who not. You think you're doing your best? Lets see here mate."
+          SrcLink=""
+          WebLink=""
+          Skills={[
+            'Typescript',
+            'React',
+            'Express',
+            'TailwindCSS',
+            'Prisma',
+            'PostgreSQL',
+            'Gemini API',
+            'Docker',
+          ]}
+        />
+
+        <Card
+          Title="Github Metrics"
+          Desc="Compare your Github Metrics with the best in the Bizz. Torvalds, Karpathy, Harkirat and who not. You think you're doing your best? Lets see here mate."
+          SrcLink="https://github.com/abhub23/Github-Metrics"
+          WebLink=""
+          Skills={[
+            'Typescript',
+            'React',
+            'Express',
+            'TailwindCSS',
+            'Prisma',
+            'PostgreSQL',
+            'Gemini API',
+            'Docker',
+          ]}
+        />
+
         <Card
           Title="React Flip game"
-          Desc="The application is about a memory game make eith typescript and react"
+          Desc="Compare your Github Metrics with the best in the Bizz. Torvalds, Karpathy, Harkirat and who not. You think you're doing your best? Lets see here mate."
+          SrcLink=""
+          WebLink=""
+          Skills={[
+            'Typescript',
+            'React',
+            'Express',
+            'TailwindCSS',
+            'Prisma',
+            'PostgreSQL',
+            'Gemini API',
+            'Docker',
+          ]}
         />
-        <Card Title="React Flip game" />
-        <Card Title="React Flip game" />
-        <Card Title="React Flip game" />
-        <Card Title="React Flip game" />
-        <Card Title="React Flip game" />
+
+        <Card
+          Title="React Flip game"
+          Desc="Created a Memory Game using React and TypeScript with dynamic grids, easy to hard levels and very cool UI to interact with."
+          SrcLink="https://github.com/abhub23/React-Flip-Game"
+          WebLink=""
+          Skills={[
+            'Typescript',
+            'React',
+            'Express',
+            'TailwindCSS',
+            'Prisma',
+            'PostgreSQL',
+            'Gemini API',
+            'Docker',
+          ]}
+        />
+
+        <Card
+          Title="React Flip game"
+          Desc="Compare your Github Metrics with the best in the Bizz. Torvalds, Karpathy, Harkirat and who not. You think you're doing your best? Lets see here mate."
+          SrcLink=""
+          WebLink=""
+          Skills={[
+            'Typescript',
+            'React',
+            'Express',
+            'TailwindCSS',
+            'Prisma',
+            'PostgreSQL',
+            'Gemini API',
+            'Docker',
+          ]}
+        />
+
+        <Card
+          Title="React Flip game"
+          Desc="Compare your Github Metrics with the best in the Bizz. Torvalds, Karpathy, Harkirat and who not. You think you're doing your best? Lets see here mate."
+          SrcLink=""
+          WebLink=""
+          Skills={[
+            'Typescript',
+            'React',
+            'Express',
+            'TailwindCSS',
+            'Prisma',
+            'PostgreSQL',
+            'Gemini API',
+            'Docker',
+          ]}
+        />
       </div>
     </>
   );
