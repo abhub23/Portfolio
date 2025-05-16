dbc6e3396e7e12f2b60763a7d7ae8372806e2e60
abdullahmukri25@gmail.com <91679434+Abdullah-Github25@users.noreply.github.com>
2025-05-16 22:28:47 +0530

    fix: some minor ui changes

--- components/Introsec.tsx
+++ components/Introsec.tsx
@@ -6,33 +6,34 @@ import Button from '../components/Button';
 
 const Introsec: React.FC = (): JSX.Element => {
   const openCal = () => {
-    window.open('https://www.github.com/abhub23', '_blank');
+    window.open('https://cal.com/abdullah23/15min?overlayCalendar=true', '_blank');
   };
 
   const scrollDown = () => {
-    window.scrollTo({ top: 1400, behavior: 'smooth' });
+    const scrollTarget = window.innerWidth > 1500 ? 1820 : 2500;
+    window.scrollTo({ top: scrollTarget, behavior: 'smooth' });
   };
 
   return (
     <div
       className={`lg:h-70 flex flex-col items-center mt-48 justify-center text-black dark:text-white bg-transparent ${Bricolage} `}
     >
-      <div className="lg:text-[42px] p-2 font-extrabold text-[26px]">Hey, I'm Abdullah Mukri</div>
+      <div className="lg:text-[42px] p-2 font-bold text-[26px]">Hey, I&apos;m Abdullah Mukri</div>
 
-      <div className="lg:text-[16px] text-sm pt-1 px-4 lg:mx-auto  ">
+      <div className="lg:text-[16px] text-[13px] pt-1 text-center px-4 lg:mx-auto">
         Hey folks Abdullah this side, a Software Engineer who loves to make cool products using
         Typescript and Go.
       </div>
 
-      <div className="lg:text-[16px] text-sm pt-1 px-4 lg:mx-auto">
+      <div className="lg:text-[16px] text-center text-[13px] pt-1 px-[14px] lg:mx-auto">
         I have expertise in wide range of tech but always want to be known as a student of Computer
         science.
       </div>
 
-      <div className="lg:text-[16px] text-sm pt-1 px-4 lg:mx-auto">
-        Want's to connect or just say hi? please don't hesitate. feel free to connect!
+      <div className="lg:text-[16px] text-center text-[13px] pt-1 px-4 lg:mx-auto">
+        Want&apos;s to connect or just say hi? please don&apos;t hesitate. feel free to connect!
       </div>
-      <div className="mt-10 flex lg:space-x-6 space-x-5">
+      <div className="lg:mt-[40px] mt-[30px] flex lg:space-x-6 space-x-4">
         <Button prop="Book a Meet" onClick={openCal} />
         <Button prop="Get in Touch" onClick={scrollDown} />
       </div>
