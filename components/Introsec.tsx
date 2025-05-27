66d71163ac8d0246c796b0d0fe1014c0fe3e658d
abdullahmukri25@gmail.com <91679434+Abdullah-Github25@users.noreply.github.com>
2025-05-28 01:20:38 +0530

    fix: intro done for both viewports

--- components/Introsec.tsx
+++ components/Introsec.tsx
@@ -3,6 +3,7 @@
 import { JSX } from 'react';
 import { Bricolage } from '@/utils/fonts';
 import Button from '../components/Button';
+import Image from 'next/image';
 
 const Introsec: React.FC = (): JSX.Element => {
   const openCal = () => {
@@ -15,29 +16,42 @@ const Introsec: React.FC = (): JSX.Element => {
   };
 
   return (
-    <div
-      className={`lg:h-70 flex flex-col items-center mt-48 justify-center text-black dark:text-white bg-transparent ${Bricolage} `}
-    >
-      <div className="lg:text-[42px] p-2 font-bold text-[26px]">Hey, I&apos;m Abdullah Mukri</div>
+    <>
 
-      <div className="lg:text-[16px] text-[13px] pt-1 text-center px-4 lg:mx-auto">
-        Hey folks Abdullah this side, a Software Engineer who loves to make cool products using
-        Typescript and Go.
-      </div>
 
-      <div className="lg:text-[16px] text-center text-[13px] pt-1 px-[14px] lg:mx-auto">
-        I have expertise in wide range of tech but always want to be known as a student of Computer
-        science.
-      </div>
+      <div
+        className={`lg:h-[500px] flex flex-col items-center lg:mt-26 mt-[120px] justify-center text-black dark:text-white bg-transparent ${Bricolage} `}
+      >
+        <div className='relative lg:h-[180px] lg:w-[180px] w-[120px] h-[160px] flex justify-center'>
+          <Image alt='me' src='/me.png'
+          onClick={() => window.open('https://github.com/abhub23', '_blank')}
+          fill
+        className="object-cover rounded-lg"
+        priority />
+        
+        </div>
+        <div className="lg:text-[42px] p-2 pb-1 font-bold text-[26px]">Hey, I&apos;m Abdullah Mukri</div>
 
-      <div className="lg:text-[16px] text-center text-[13px] pt-1 px-4 lg:mx-auto">
-        Want&apos;s to connect or just say hi? please don&apos;t hesitate. feel free to connect!
+        <div className="lg:text-[16px] text-[13px] pt-1 text-center px-4 lg:mx-auto">
+          Hey folks Abdullah this side, a Software Engineer who loves to make cool products using
+          Typescript and Go.
+        </div>
+
+        <div className="lg:text-[16px] text-center text-[13px] pt-1 px-[14px] lg:mx-auto">
+          I have expertise in wide range of tech but always want to be known as a student of Computer
+          science.
+        </div>
+
+        <div className="lg:text-[16px]  text-center text-[13px] pt-1 px-4 lg:mx-auto">
+          Want&apos;s to connect or just say hi? please don&apos;t hesitate. feel free to connect!
+        </div>
+        <div className="lg:mt-[40px] mt-[30px] flex lg:space-x-6 space-x-4">
+          <Button prop="Book a Meet" onClick={openCal} />
+          <Button prop="Get in Touch" onClick={scrollDown} />
+        </div>
       </div>
-      <div className="lg:mt-[40px] mt-[30px] flex lg:space-x-6 space-x-4">
-        <Button prop="Book a Meet" onClick={openCal} />
-        <Button prop="Get in Touch" onClick={scrollDown} />
-      </div>
-    </div>
+    </>
+
   );
 };
 
