09deeb12b6d5339bf807b88d4a31611163b70f16
abdullahmukri25@gmail.com <91679434+Abdullah-Github25@users.noreply.github.com>
2025-05-30 23:53:19 +0530

    some blur effect

--- app/page.tsx
+++ app/page.tsx
@@ -12,9 +12,9 @@ export default function Home() {
   return (
     <>
       <motion.div
-        initial={{ opacity: 0, y: 30 }}
-        animate={{ opacity: 1, y: 0 }}
-        transition={{ duration: 0.6, ease: 'easeOut' }}
+        initial={{ opacity: 0, y: 30, filter: 'blur(8px)' }}
+        animate={{ opacity: 1, y: 0, filter: 'blur(0px)'  }}
+        transition={{ duration: 0.4, ease: 'easeOut' }}
       >
         <Introsec />
         <Projects />
