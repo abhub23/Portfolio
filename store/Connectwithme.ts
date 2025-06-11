43cba4879dbe460cc91afe92a4c60881efe9a138
abdullahmukri25@gmail.com <91679434+Abdullah-Github25@users.noreply.github.com>
2025-06-11 23:40:24 +0530

    feat: state added

--- store/Connectwithme.ts
+++ store/Connectwithme.ts
@@ -9,7 +9,6 @@ export const useEmail = create<Email>((set) => ({
   setEmail: (newemail: string) => set({ email: newemail }),
 }));
 
-
 type Message = {
   message: string;
   setMessage: (newmessage: string) => void;
@@ -17,4 +16,13 @@ type Message = {
 export const useMessage = create<Message>((set) => ({
   message: '',
   setMessage: (newmessage: string) => set({ message: newmessage }),
+}));
+
+type SendMessage = {
+  sendMessage: boolean;
+  setSendMessage: (newmessage: boolean) => void;
+};
+export const useSendMessage = create<SendMessage>((set) => ({
+  sendMessage: false,
+  setSendMessage: (newmessage: boolean) => set({ sendMessage: newmessage }),
 }));
