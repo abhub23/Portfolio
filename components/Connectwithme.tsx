aa5b9bf34aa5e01f72cd09874d86ff6cdc3693fe
abdullahmukri25@gmail.com <91679434+Abdullah-Github25@users.noreply.github.com>
2025-06-15 05:57:07 +0530

    hope it dont break prod

--- components/Connectwithme.tsx
+++ components/Connectwithme.tsx
@@ -73,7 +73,7 @@ const Connectwithme: React.FC = (): JSX.Element => {
       className={` ${Bricolage} flex h-[380px] flex-col items-start p-4 lg:ml-100 lg:h-[460px] lg:w-[750px]`}
     >
       <div className={`mx-auto mb-2 text-[24px] font-semibold lg:mb-5 lg:text-[34px]`}>
-        Connect with ab
+        Connect with me
       </div>
 
       <div className="mb-1 flex h-[40px] w-full justify-start gap-x-[8px] p-1 lg:h-[50px] lg:gap-x-[12px]">
