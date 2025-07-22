44d3ef7d7a8a26f1313009e4f7781a7e0fd41285
abdullahmukri25@gmail.com <91679434+Abdullah-Github25@users.noreply.github.com>
2025-07-22 22:06:51 +0530

    fix: fixed the mapping and file structure

---
+++ components/project/Loadbtn.tsx
@@ -0,0 +1,22 @@
+import { Bricolage } from '@/utils/fonts';
+import { FC, JSX } from 'react';
+
+type BtnType = {
+  name: string
+  onClick: () => void;
+  icon: JSX.Element
+};
+
+export const Loadbtn: FC<BtnType> = ({ name, onClick, icon }) => {
+  return (
+    <button
+      onClick={onClick}
+      className={`flex cursor-pointer rounded-[3px] border-1 border-black bg-black/90 px-[6px] py-[1px] font-medium text-white lg:text-[12px] dark:bg-white dark:text-black ${Bricolage}`}
+    >
+      {name}
+      <span className="p-[3px]">
+        {icon}
+      </span>
+    </button>
+  );
+};
