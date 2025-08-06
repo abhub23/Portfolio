28dd6b4ca7148cc552191c565fcc8bd1e4e86f8f
abdullahmukri25@gmail.com <91679434+Abdullah-Github25@users.noreply.github.com>
2025-08-07 02:39:18 +0530

    refactor

--- components/project/Card.tsx
+++ components/project/Card.tsx
@@ -1,4 +1,4 @@
-import React, { JSX } from 'react';
+import React, { FC, JSX } from 'react';
 import GithubBtn from './GithubBtn';
 import WebsiteBtn from './WebsiteBtn';
 import { Bricolage } from '@/utils/fonts';
@@ -14,16 +14,11 @@ type Cardprops = {
   Imglink: React.HTMLInputTypeAttribute;
 };
 
-const Card: React.FC<Cardprops> = ({
-  Title,
-  Desc,
-  SrcLink,
-  WebLink,
-  Skills,
-  Imglink,
-}): JSX.Element => {
+const Card: FC<Cardprops> = ({ Title, Desc, SrcLink, WebLink, Skills, Imglink }): JSX.Element => {
   return (
-    <article className="relative flex w-[310px] flex-col items-center justify-between rounded-sm border-[1px] border-zinc-300/80 bg-white p-6 lg:w-[340px] dark:border-zinc-800 dark:bg-black">
+    <article
+      className={`relative flex w-[310px] flex-col items-center justify-between rounded-sm border-[1px] border-zinc-300/80 bg-neutral-50 p-6 lg:w-[340px] dark:border-zinc-800 dark:bg-neutral-950 ${Bricolage}`}
+    >
       <ShineBorder
         className="rounded-sm"
         shineColor={[
@@ -34,11 +29,9 @@ const Card: React.FC<Cardprops> = ({
       />
       <div className="group relative w-[290px]">
         <div className="flex items-center justify-start">
-          <h3
-            className={`mt-[4px] ml-[8px] transform text-[18px] leading-6 font-semibold text-black transition-all duration-500 ease-in-out hover:scale-105 hover:text-rose-600 lg:w-fit dark:text-white ${Bricolage}`}
-          >
+          <h3 className="mt-[4px] ml-[8px] transform text-[18px] leading-6 font-semibold text-black transition-all duration-500 ease-in-out hover:scale-105 hover:text-rose-600 lg:w-fit dark:text-white">
             <a href={WebLink}>
-              <span className={`absolute inset-0 max-w-xs ${Bricolage}`} />
+              <span className="absolute inset-0 max-w-xs" />
               {Title}
             </a>
           </h3>
@@ -52,9 +45,7 @@ const Card: React.FC<Cardprops> = ({
           />
         </div>
 
-        <p
-          className={`mt-5 px-2 text-justify text-[11px] text-black lg:text-[13px] dark:text-white ${Bricolage} `}
-        >
+        <p className="mt-5 px-2 text-justify text-[11px] text-black lg:text-[13px] dark:text-white">
           {Desc}
         </p>
       </div>
@@ -63,7 +54,7 @@ const Card: React.FC<Cardprops> = ({
         {Skills.map((el) => (
           <p
             key={el}
-            className={`ml-[2px] h-[18px] cursor-pointer rounded-[3px] border-1 border-neutral-500 p-[2px] px-[4px] text-[10px] text-black transition-all duration-300 ease-in-out hover:bg-black hover:text-white lg:h-[20px] lg:text-[11px] dark:border-neutral-500 dark:text-white dark:hover:bg-white hover:dark:text-black ${Bricolage}`}
+            className="ml-[2px] h-[18px] cursor-pointer rounded-[3px] border-1 border-neutral-500 p-[2px] px-[4px] text-[10px] text-black transition-all duration-300 ease-in-out hover:bg-black hover:text-white lg:h-[20px] lg:text-[11px] dark:border-neutral-500 dark:text-white dark:hover:bg-white hover:dark:text-black"
           >
             {el}
           </p>
