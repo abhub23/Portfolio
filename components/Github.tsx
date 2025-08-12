8a6b7c4e390d08afdc0d9f45d992c2ae0d12dfbb
abdullahmukri25@gmail.com <91679434+Abdullah-Github25@users.noreply.github.com>
2025-08-12 09:18:01 +0530

    some image optimization of chart

--- components/Github.tsx
+++ components/Github.tsx
@@ -1,19 +1,19 @@
 import { Bricolage } from '@/utils/fonts';
 import Image from 'next/image';
-import type { JSX } from 'react';
 
-const Github: React.FC = (): JSX.Element => {
+const Github = () => {
   return (
     <div className="mt-8 mb-2 flex flex-col items-center p-3 lg:mt-11 lg:mb-10">
       <p className={`${Bricolage} mb-2 text-[20px] font-semibold lg:mb-5 lg:text-[30px]`}>
-        Github Contributions
+        Github Activities
       </p>
       <Image
         src="https://ghchart.rshah.org/158a17/abhub23"
-        alt="Github contribution Chart"
+        alt="Github Chart"
         width={780}
         height={500}
         unoptimized={true}
+        loading='lazy'
       />
     </div>
   );
