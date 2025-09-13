7980a1f277adaeae5c9538c74e8f51c15085aba6
abdullahmukri25@gmail.com <91679434+Abdullah-Github25@users.noreply.github.com>
2025-09-13 22:56:34 +0530

    refactor: shifted types to types folder

--- store/Connectwithme.ts
+++ store/Connectwithme.ts
@@ -1,27 +1,16 @@
 import { create } from 'zustand';
+import { Email, Message, SendMessage } from '@/types/types';
 
-type Email = {
-  email: string;
-  setEmail: (newemail: string) => void;
-};
 export const useEmail = create<Email>((set) => ({
   email: '',
   setEmail: (newemail: string) => set({ email: newemail }),
 }));
 
-type Message = {
-  message: string;
-  setMessage: (newmessage: string) => void;
-};
 export const useMessage = create<Message>((set) => ({
   message: '',
   setMessage: (newmessage: string) => set({ message: newmessage }),
 }));
 
-type SendMessage = {
-  sendMessage: boolean;
-  setSendMessage: (newmessage: boolean) => void;
-};
 export const useSendMessage = create<SendMessage>((set) => ({
   sendMessage: false,
   setSendMessage: (newmessage: boolean) => set({ sendMessage: newmessage }),
