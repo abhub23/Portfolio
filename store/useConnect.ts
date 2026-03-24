884235bc54f087f27133b3ea58ee8f1a6780602c
abdullahmukri25@gmail.com <91679434+Abdullah-Github25@users.noreply.github.com>
2026-03-24 22:24:09 +0530

    add useConnect store

---
+++ store/useConnect.ts
@@ -0,0 +1,14 @@
+import { create } from 'zustand';
+import type ConnectType from '@/types/state-types';
+
+const useConnect = create<ConnectType>((set) => ({
+  email: '',
+  message: '',
+  sendMessage: false,
+  setEmail: (newemail: string) => set({ email: newemail }),
+  setMessage: (newmessage: string) => set({ message: newmessage }),
+  setSendMessage: (newmessage: boolean) => set({ sendMessage: newmessage }),
+
+}));
+
+export default useConnect
