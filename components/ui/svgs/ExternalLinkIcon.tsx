561903132137f7454ab4b306f90ffd9496f10585
abdullahmukri25@gmail.com <91679434+Abdullah-Github25@users.noreply.github.com>
2026-04-07 01:55:27 +0530

    seperate logos

---
+++ components/ui/svgs/ExternalLinkIcon.tsx
@@ -0,0 +1,22 @@
+import type { SVGProps } from "react";
+
+type IconProps = SVGProps<SVGSVGElement>;
+
+export const ExternalLinkIcon = (props: IconProps) => (
+  <svg
+    width={13}
+    height={13}
+    viewBox="0 0 24 24"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    {...props}
+  >
+    <path
+      d="M7 17L17 7M17 7H7M17 7V17"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+  </svg>
+);
